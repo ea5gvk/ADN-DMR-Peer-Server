3066964cc1f0162d81bf5bf1cc2c83456bae3137
Esteban Mackay Q. <49044505+hp3icc@users.noreply.github.com>
2025-04-06 20:40:12 -0500

    Update docker-compose_install2.sh
    auto change tag rpi

--- docker-configs/docker-compose_install2.sh
+++ docker-configs/docker-compose_install2.sh
@@ -292,6 +292,14 @@ echo Get docker-compose.yml...
 cd /etc/ADN-Systems &&
 curl https://raw.githubusercontent.com/Amateur-Digital-Network/ADN-DMR-Peer-Server/develop/docker-configs/docker-compose2.yml -o docker-compose.yml &&
 
+if [ "$(cat /proc/cpuinfo | grep 'Raspberry')" != "" ]; then
+sed -i "s/cpu_shares: 1024/#cpu_shares: 1024/g"  /etc/ADN-Systems/docker-compose.yml
+sed -i "s/mem_reservation: 600m/#mem_reservation: 600m/g"  /etc/ADN-Systems/docker-compose.yml
+sed -i "s/adn-server-self-service:latest/adn-server-self-service:rpi/g"  /etc/ADN-Systems/docker-compose.yml
+sed -i "s/adn-mon2-selfservice:latest/adn-mon2-selfservice:rpi/g"  /etc/ADN-Systems/docker-compose.yml
+sed -i "s/cpu_shares: 512/#cpu_shares: 512/g"  /etc/ADN-Systems/docker-compose.yml
+fi
+
 chmod 755 /etc/cron.daily/lastheard
 
 echo Tune network stack...
